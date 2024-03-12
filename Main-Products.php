3ff0478a94efde38eaff37d0573f4a0a84e63c49
JerrelLuna <lerrejluna@gmail.com>
2024-03-12 12:15:19 +0800

    Update Main-Products.php

--- Main-Products.php
+++ Main-Products.php
@@ -15,7 +15,7 @@
    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500;600&family=Roboto:wght@300;400;700&display=auto"
      rel="stylesheet">
  
-   <!-- Favicon -->
+   <!-- Favicon TESTINGGGGGGGGGGGGGGGasdfasdfasdfasdfsasdfasdfasdfsad-->
    <link rel="apple-touch-icon" sizes="180x180" href="./assets/images/favicon/apple-touch-icon.png">
    <link rel="icon" type="image/png" sizes="32x32" href="./assets/images/favicon/favicon-32x32.png">
    <link rel="icon" type="image/png" sizes="16x16" href="./assets/images/favicon/favicon-16x16.png">
