9b4c164cc6f6a6df70ea49bb3d957c670d522aa4
Paopaopawi <148065857+Paopaopawi@users.noreply.github.com>
2024-03-15 14:19:38 +0800

    slight changes

--- index.php
+++ index.php
@@ -18,12 +18,133 @@
 
 <!-- Main CSS -->
 <link rel="stylesheet" href="./assets/css/theme.bundle.css" />
-<?php include 'header.php'?>
 <link rel="stylesheet" href="style.css">
- 
+
+
 </head>
 <body>
+        <!-- Navbar -->
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light  flex-column border-0  ">
+      <div class="container-fluid">
+          <div class="w-100">
+              <div class="d-flex justify-content-between align-items-center flex-wrap">
   
+                  <!-- Logo-->
+                  <a class="navbar-brand fw-bold fs-3 m-0 p-0 flex-shrink-0 order-0" href="index.php">
+                      <div class="d-flex align-items-center">
+                          <img src="images\logo.png">
+                      </div>
+                  </a>
+                  <!-- / Logo-->
+  
+                  <!-- Navbar Icons-->
+                  <ul class="list-unstyled mb-0 d-flex align-items-center order-1 order-lg-2 nav-sidelinks">
+  
+                      <!-- Mobile Nav Toggler-->
+                      <li class="d-lg-none">
+                          <span class="nav-link  d-flex align-items-center cursor-pointer text-light" data-bs-toggle="collapse"
+                              data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                              aria-label="Toggle navigation"><i class="ri-menu-line ri-lg me-1  text-light"></i> Menu</span>
+                      </li>
+                      <!-- /Mobile Nav Toggler-->
+  
+                      <!-- Navbar Search-->
+                      <li class="d-none d-sm-block">
+                          <span class="nav-link text-light search-trigger cursor-pointer">Search</span>
+  
+                          <!-- Search navbar overlay-->
+                          <div class="navbar-search d-none">
+                              <div class="input-group mb-3 h-100">
+                                  <span class="input-group-text px-4 bg-transparent border-0"><i
+                                          class="ri-search-line ri-lg"></i></span>
+                                  <input type="text" class="form-control text-body bg-transparent border-0"
+                                      placeholder="Enter your search terms...">
+                                  <span
+                                      class="input-group-text px-4 cursor-pointer disable-child-pointer close-search bg-transparent border-0"><i
+                                          class="ri-close-circle-line ri-lg"></i></span>
+                              </div>
+                          </div>
+                          <div class="search-overlay"></div>
+                          <!-- / Search navbar overlay-->
+  
+                      </li>
+                      <!-- /Navbar Search-->
+  
+                      <!-- Navbar Login-->
+                      <li class="ms-1 d-none d-lg-inline-block  ">
+                          <a class="nav-link text-light" href="login.php">
+                              Login
+                          </a>
+                      </li>
+                      <!-- /Navbar Login-->
+  
+                      <!-- Navbar Cart Icon-->
+                      <li class="ms-1 d-inline-block position-relative dropdown-cart">
+                          <button class="nav-link me-0 disable-child-pointer border-0 p-0 bg-transparent text-light"
+                              type="button ">
+                              Bag (2)
+                          </button>
+                          <div class="cart-dropdown dropdown-menu">
+                          
+                              <!-- Cart Header-->
+                              <div class="d-flex justify-content-between align-items-center border-bottom pt-3 pb-4">
+                                  <h6 class="fw-bolder m-0">Cart Summary</h6>
+                                  <i class="ri-close-circle-line text-muted ri-lg cursor-pointer btn-close-cart"></i>
+                              </div>
+                              <!-- / Cart Header-->
+                          
+                              <!-- Cart Items-->
+                              <div>
+                          
+                                 
+                            </div>
+                          
+  
+                      </li>
+                      <!-- /Navbar Cart Icon-->
+  
+                  </ul>
+                  <!-- Navbar Icons-->                
+  
+                  <!-- Main Navigation-->
+                  <div class="flex-shrink-0 collapse navbar-collapse navbar-collapse-black w-auto flex-grow-1 order-2 order-lg-1"
+                      id="navbarNavDropdown">
+
+                      <!-- Menu-->
+                      <ul class="navbar-nav mx-auto mb-2 mb-lg-0  ">
+                        <li class="nav-item">
+                          <a class="nav-link text-white" href="index.php" role="button">
+                            Home
+                          </a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link text-white" href="about-page.php" role="button">
+                            About Us
+                          </a>
+                        </li>
+                          <li class="nav-item dropdown dropdown position-static">
+                            <a class="nav-link dropdown-toggle text-white"   href="Main-Products.php" role="button"  aria-expanded="false">
+                              Products
+                            </a>
+                          </li>
+                          
+                          <li class="nav-item">
+                            <a class="nav-link text-white" href="contact.php" role="button">
+                              Contact us
+                            </a>
+                          </li>
+                              
+                            
+                                               <!-- / Menu-->
+  
+                  </div>
+
+              </div>
+          </div>
+      </div>
+  </nav>
+  <!-- / Navbar-->    <!-- / Navbar-->
          <!-- Page Content Goes Here -->
         <!-- / Top banner -->
         <section class="vh-75 vh-lg-60 container-fluid rounded overflow- mt-4" data-aos="fade-in">
@@ -156,7 +277,6 @@
     <img src="images\banner22.png" />
     <img src="images\banner33.png" />
     <img src="images\banner11.png" />
->>>>>>> Stashed changes
   </div>
 </div>
 <!--/UNLI BANNER-->
@@ -218,14 +338,23 @@
         <!--/Third Page-->
 
         <!--Fourth Page-->
-        
+
         <!--/Fourth Page-->
+
+
+
+
+
+
     <!-- Theme JS -->
     <!-- Vendor JS -->
     <script src="assets\js\vendor.bundle.js"></script>
     
     <!-- Theme JS -->
     <script src="assets\js\theme.bundle.js"></script>
-    
+
+
+
+
 </body>
 </html>
